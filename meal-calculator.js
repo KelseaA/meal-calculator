dd47219b465c03e320b4a19959f88a716ca14597
Kelsea <andersonkelsea@gmail.com>
2016-05-10 19:56:06 -0400

    Adding bill total and breakdown

--- meal-calculator.js
+++ meal-calculator.js
@@ -1,6 +1,6 @@
 // sets up diner object
 var Diner = function(dinerName){
-	this.dinerName = dinerName;
+  this.dinerName = dinerName;
   var meal = new Meal();
   this.getMealTotal = function(){
     return meal.calculateTotalAmount();
@@ -8,32 +8,42 @@ var Diner = function(dinerName){
   this.addDishToMeal = function(dish){
     meal.addDish(dish);
   }
+  this.printMealBreakdown = function(){
+    meal.getDishes().forEach(function(dish){
+      console.log("Dish Name: " + dish.getName());
+      console.log("Price: $" + round(dish.getPrice()));
+      console.log("Tax: $" + round(dish.getPricePlusTax() - dish.getPrice()));
+      console.log("Total: $" + round(dish.getPricePlusTax()));
+    })
+  }
 };
 
 // sets up meal object
 var Meal = function(){
-	var dishes = [];
+  var dishes = [];
   // adds dishes to dishes array in meal
-  	this.addDish = function(dish){
-  		dishes.push(dish);
-  	};
-
+    this.addDish = function(dish){
+      dishes.push(dish);
+    };
+    this.getDishes = function(){
+      return dishes;
+    }
 // calculates cost of dishes in a meal and adds to total
-	this.calculateTotalAmount = function (){
-		var total = 0;
+  this.calculateTotalAmount = function (){
+    var total = 0;
 
-		dishes.forEach(function(dish){
-			total += dish.getPricePlusTax();
-		});
+    dishes.forEach(function(dish){
+      total += dish.getPricePlusTax();
+    });
 
     return total;
-	};
+  };
 };
 
 // sets up dish object
 var Dish = function(dishName, dishPrice){
-	var name = dishName;
-	var price = dishPrice;
+  var name = dishName;
+  var price = dishPrice;
   var tax = 0.07;
   this.getPrice = function(){
     return price;
@@ -57,14 +67,25 @@ var Bill = function(){
     diners.forEach(function(diner){
       total += diner.getMealTotal();
     });
-	  return total;
+    return total;
+  }
+  this.calculateTipPerPerson = function(){
+    return round((this.calculateTotalAmount() * tip) / diners.length);
   }
   this.calculateTotalAmountPlusTip = function(){
     return this.calculateTotalAmount() + (this.calculateTotalAmount() * tip);
   }
   this.printBillTotal = function(){
-    console.log(this.calculateTotalAmountPlusTip());
+    console.log("The total bill is: $" + this.calculateTotalAmountPlusTip());
   }
+  this.printBillBreakdown = function(){
+    diners.forEach(function(diner){
+      console.log("Name: " + diner.dinerName);
+      diner.printMealBreakdown();
+      console.log("Tip: $" + that.calculateTipPerPerson());
+    })
+  }
+  var that = this;
 }
 
 
@@ -90,6 +111,13 @@ bill.addDiner(diner1);
 bill.addDiner(diner2);
 
 bill.printBillTotal();
+
+bill.printBillBreakdown();
+
+function round(number){
+  return (Math.round(number*Math.pow(10,2))/Math.pow(10,2)).toFixed(2);
+}
+
 
 
 
